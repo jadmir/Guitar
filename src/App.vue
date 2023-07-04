ba99692781e31ef96dc6de96994eda9cf3970cd4
Jadmir <jadmir1111@gmail.com>
2023-07-04 12:36:46 -0500

    segundo envio

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,55 @@
   import {ref, reactive, onMounted} from 'vue'
   import { db } from './data/guitarras'
   import Guitarra from './components/Guitarra.vue'
-  import Header from './components/header.vue'
+  import Header from './components/Header.vue'
   import Footer from './components/Footer.vue'
 
   const guitarras = ref([])
   const carrito = ref([])
+  const guitarra = ref({})
 
   onMounted(() => {
     guitarras.value = db;
+    guitarra.value = db[3];
   })
 
   const agregarCarrito = (guitarra) => {
-      guitarra.cantidad = 1;
-      carrito.value.push(guitarra);
+      const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+      if(existeCarrito >= 0) {
+        carrito.value[existeCarrito].cantidad++
+      } else {
+        guitarra.cantidad = 1;
+        carrito.value.push(guitarra);
+      }
     }
+
+    const decrementarCantidad = (id) => {
+      const index = carrito.value.findIndex(producto => producto.id === id)
+      if(carrito.value[index].cantidad <= 1) return
+      carrito.value[index].cantidad--
+    }
+
+  const incrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if(carrito.value[index].cantidad >= 5) return
+    carrito.value[index].cantidad++
+  }
+
+  const eliminarProducto = (id) => {
+    carrito.value = carrito.value.filter(producto => producto.id !== id)
+  }
 </script>
 
 <template>
-    <Header />
+    <Header 
+      :carrito="carrito"
+      :guitarra="guitarra"
+      @decrementar-cantidad="decrementarCantidad"
+      @incrementar-cantidad="incrementarCantidad"
+      @agregar-carrito="agregarCarrito"
+      @eliminar-producto="eliminarProducto"
+    />
+
 
     <main class="container-xl mt-5">
         <h2 class="text-center">Nuestra Colección</h2>
